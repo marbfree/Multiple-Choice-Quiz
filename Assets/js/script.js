864c832eb1b4143b5116f346ffbf621f0a8f417e
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-05-03 15:27:56 -0400

    added styling to css and javascript files

---
+++ Assets/js/script.js
@@ -0,0 +1,12 @@
+let quizTitle = document.getElementById("quizName");
+let eachQuestion = document.getElementById("question");
+let answerKey = document.getElementsByClassName("button");
+let submitBtn = document.getElementById("btn");
+
+quizTitle.setAttribute("style", "margin: 30px; font-weight: bold; font-size: 50px; color: #00008b;");
+eachQuestion.setAttribute("style", "margin-bottom: 50px; font: bolder; font-size: 30px;");
+answerKey[0].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 25% 10%;");
+answerKey[1].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 10px 100px / 120px;");
+answerKey[2].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 25% 10%;");
+answerKey[3].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 10px 100px / 120px;");
+submitBtn.setAttribute("style", "border: solid #00008b 2px; background-color: #FF7F50; border-radius: 10% / 50%;");
