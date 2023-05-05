8e18171a6749d25cd7e52b8f48f2ae872e661e3b
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-05-05 10:20:10 -0400

    added working link to button on high scores page and updated README file

--- Assets/js/highscores.js
+++ Assets/js/highscores.js
@@ -1,7 +1,14 @@
-returnBtn = document.getElementsByClassName("btn");
-titleHS = document.querySelector("h1")
+let returnBtn = document.getElementsByClassName("btn");
+let titleHS = document.querySelector("h1");
 
 
 returnBtn[0].setAttribute("style", "border: solid #00008b 2px; background-color: #FF7F50; border-radius: 10% / 50%; color: #00008b; padding: 10px 10px;");
 returnBtn[1].setAttribute("style", "border: solid #00008b 2px; background-color: #FF7F50; border-radius: 10% / 50%; color: #00008b;");
 titleHS.setAttribute("style", "border-bottom: solid 2px #FF7F50; margin: 30px; font-weight: bold; font-size: 50px; color: #00008b;");
+
+function gotoLink(link){
+    console.log(link.value);
+    location.href="/index.html";
+}
+
+// returnBtn[1].addEventListener("click", 
---
+++ READMENew.md
@@ -0,0 +1,5 @@
+#Acknowledgements
+
+Thank you Chart JS for your tutorial on how to create a button with a link in Javascript.  I was helped with line 17 in HTML and the function on line 9 in JavaScript. See link below for the video tutorial. 
+
+https://www.google.com/search?q=how+to+add+a+link+to+a+function+in+javascript&oq=how+to+add+a+link+to+a+function&aqs=chrome.0.69i59j69i57.6153j0j4&sourceid=chrome&ie=UTF-8#fpstate=ive&vld=cid:8f4adf75,vid:xVMkFJZhZYU
