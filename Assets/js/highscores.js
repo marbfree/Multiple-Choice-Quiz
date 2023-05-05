f892e79288f0c3d70cafc1550d568d2ebee63b8e
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-05-05 09:25:40 -0400

    added link and style to High Scores page

---
+++ Assets/js/highscores.js
@@ -0,0 +1,7 @@
+returnBtn = document.getElementsByClassName("btn");
+titleHS = document.querySelector("h1")
+
+
+returnBtn[0].setAttribute("style", "border: solid #00008b 2px; background-color: #FF7F50; border-radius: 10% / 50%; color: #00008b; padding: 10px 10px;");
+returnBtn[1].setAttribute("style", "border: solid #00008b 2px; background-color: #FF7F50; border-radius: 10% / 50%; color: #00008b;");
+titleHS.setAttribute("style", "border-bottom: solid 2px #FF7F50; margin: 30px; font-weight: bold; font-size: 50px; color: #00008b;");
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,9 @@
 let quizTitle = document.getElementById("quizName");
-let eachQuestion = document.getElementById("question");
+let titleQuestion = document.getElementById("question");
 let answerKey = document.getElementsByClassName("button");
-let submitBtn = document.getElementById("btn");
+let submitBtn = document.querySelector(".btn");
 let scores = document.querySelector("header");
-let countTimer = document.getElementById("timer");
+let countTimer = document.querySelector("#timer");
 
 // window.confirm("You have 75 seconds to answer 5 questions.  \nAre you ready to Play?")
 
@@ -12,11 +12,12 @@ let timeLeft = 76;
 function setTime(){
   let timerInterval = setInterval(function(){
     timeLeft--;
-    countTimer.textContent = "Time Left: " + timeLeft;
+    countTimer.textContent = "Seconds Left: " + timeLeft;
     
     if(timeLeft === 0) {
       clearInterval(timerInterval);}
     }, 1000);
+
 }
 
 setTime();
@@ -24,12 +25,12 @@ setTime();
 window.addEventListener("contextmenu", e => e.preventDefault());
 
 quizTitle.setAttribute("style", "border-bottom: solid 2px #FF7F50; margin: 30px; font-weight: bold; font-size: 50px; color: #00008b;");
-eachQuestion.setAttribute("style", "margin-bottom: 50px; font: bolder; font-size: 30px;");
-answerKey[0].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 25% 10%;");
-answerKey[1].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 10px 100px / 120px;");
-answerKey[2].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 25% 10%;");
-answerKey[3].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 10px 100px / 120px;");
-submitBtn.setAttribute("style", "border: solid #00008b 2px; background-color: #FF7F50; border-radius: 10% / 50%;");
-scores.setAttribute("style", "color: #00008b; text-decoration: none;");
+// titleQuestion.setAttribute("style", "margin-bottom: 50px; font: bolder; font-size: 30px;");
+// answerKey[0].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 25% 10%;");
+// answerKey[1].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 10px 100px / 120px;");
+// answerKey[2].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 25% 10%;");
+// answerKey[3].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 10px 100px / 120px;");
+submitBtn.setAttribute("style", "border: solid #00008b 2px; background-color: #FF7F50; border-radius: 10% / 50%; color: #00008b;");
+// scores.setAttribute("style", "color: #00008b; text-decoration: none;");
 countTimer.setAttribute("style", "color: #00008b;");
 
