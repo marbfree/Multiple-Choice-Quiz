0417dfb16191b46d34b4c7f662a926dfdddf7920
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-05-05 18:18:34 -0400

    getting help via AskBCS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,19 +1,65 @@
 let quizTitle = document.getElementById("quizName");
 let titleQuestion = document.getElementById("question");
 let answerKey = document.getElementsByClassName("button");
-let submitBtn = document.querySelector(".btn");
+let quizStart = document.querySelector(".btn");
 let scores = document.querySelector("header");
 let countTimer = document.querySelector("#timer");
-let eachRound = document.querySelector(".round")
+// let startGame = document.getElementsByClassName("homePage");
+// let eachRound = document.getElementsByClassName("round")
+let timeLeft = 76;
 
 window.addEventListener("contextmenu", e => e.preventDefault());
-submitBtn.addEventListener("click", setTime)
+// quizStart.addEventListener("click", showQuestion);
+quizStart.addEventListener("click", setTime);
 
-function showQuestion(){
-  if 
+// quizStart.addEventListener("click", function hidePage(){
+//   if (startGame.style.visibility === visible){
+//   startGame.style.visibility = hidden;
+//   } else
+//   startGame.style.visibility = visible
+// });
+
+// quizStart.addEventListener("click", gameStart)
+
+// function showQuestion() {
+//   if (eachRound.style.display === "none") {
+//     eachRound.style.display = "block";
+//   } else {
+//     eachRound.style.display = "none";
+//   }
+// };
+
+function setTime(){
+  let timerInterval = setInterval(function(){
+    timeLeft--;
+    countTimer.textContent = "Seconds Left: " + timeLeft;
+
+    if (timeLeft === 0) {
+      clearInterval(timerInterval);}
+    }, 1000);
 }
 
-// submitBtn.addEventListener("click", function(event){
+
+
+quizTitle.setAttribute("style", "border-bottom: solid 2px #FF7F50; margin: 30px; font-weight: bold; font-size: 50px; color: #00008b;");
+countTimer.setAttribute("style", "color: #00008b;");
+quizStart.setAttribute("style", "border: solid #00008b 2px; background-color: #FF7F50; border-radius: 10% / 50%; color: #00008b;");
+// quizStart[1].setAttribute("style", "border: solid #00008b 2px; background-color: #FF7F50; border-radius: 10% / 50%; color: #00008b;");
+scores.setAttribute("style", "color: #00008b; text-decoration: none;");
+
+titleQuestion.setAttribute("style", "margin-bottom: 50px; font: bolder; font-size: 30px;");
+answerKey[0].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 25% 10%;");
+answerKey[1].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 10% 25%;");
+answerKey[2].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 25% 10%;");
+answerKey[3].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 10% 25%;");
+answerKey[4].setAttribute("style", "padding-left: 30px; padding-right: 30px; background-color: #00008b; color: white; border: solid #FF7F50 2px;");
+
+
+// function showQuestion(){
+//   if 
+// }
+
+// quizStart.addEventListener("click", function(event){
 //   var element = event.target;
 
 //   if (element.matches(answerKey)){
@@ -31,30 +77,3 @@ function showQuestion(){
 
 // when play button is clicked, i want to unhide the next question
 // i want to keep the header, h1 and aside element on each page
-
-let timeLeft = 76;
-
-function setTime(){
-  let timerInterval = setInterval(function(){
-    timeLeft--;
-    countTimer.textContent = "Seconds Left: " + timeLeft;
-
-    if (timeLeft === 0) {
-      clearInterval(timerInterval);}
-    }, 1000);
-}
-
-
-
-quizTitle.setAttribute("style", "border-bottom: solid 2px #FF7F50; margin: 30px; font-weight: bold; font-size: 50px; color: #00008b;");
-countTimer.setAttribute("style", "color: #00008b;");
-submitBtn.setAttribute("style", "border: solid #00008b 2px; background-color: #FF7F50; border-radius: 10% / 50%; color: #00008b;");
-// submitBtn[1].setAttribute("style", "border: solid #00008b 2px; background-color: #FF7F50; border-radius: 10% / 50%; color: #00008b;");
-scores.setAttribute("style", "color: #00008b; text-decoration: none;");
-
-titleQuestion.setAttribute("style", "margin-bottom: 50px; font: bolder; font-size: 30px;");
-answerKey[0].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 25% 10%;");
-answerKey[1].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 10% 25%;");
-answerKey[2].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 25% 10%;");
-answerKey[3].setAttribute("style", "padding-left: 10px; padding-right: 20px; background-color: #00008b; color: white; border: solid #FF7F50 2px; border-radius: 10% 25%;");
-answerKey[4].setAttribute("style", "padding-left: 30px; padding-right: 30px; background-color: #00008b; color: white; border: solid #FF7F50 2px;");
