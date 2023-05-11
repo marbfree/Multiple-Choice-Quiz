03a8cc55057981a5d57502c87607e9a336300bcb
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-05-11 14:26:23 -0400

    changed high scores and input layout

--- Assets/js/highscores.js
+++ Assets/js/highscores.js
@@ -1,21 +1,18 @@
 let returnBtn = document.getElementsByClassName("btn");
 let titleHS = document.querySelector("h1");
-let subScore = document.querySelector("#subScore");
+// let subScore = document.querySelector("#subScore");
 let showHighScores = document.querySelector(".display");
-let enterEl = document.querySelector(".enterScore");
+// let enterEl = document.querySelector(".enterScore");
 let displayScore = document.querySelector("#myScore");
 let yourScore = document.querySelector("#scored");
 let userInScore = localStorage.getItem("correct");
 console.log(userInScore);
 let userScoresArray = JSON.parse(localStorage.getItem("userInScore")) || []
 
+
 // sets style to buttons and header
 returnBtn[0].setAttribute("style", "border: solid #00008b 2px; background-color: #FF7F50; border-radius: 10% / 50%; color: #00008b; padding: 10px 10px;");
-returnBtn[1].setAttribute("style", "border: solid #00008b 2px; background-color: #FF7F50; border-radius: 10% / 50%; color: #00008b; padding: 10px 10px; width: 10%;");
 titleHS.setAttribute("style", "border-bottom: solid 2px #FF7F50; margin: 30px; font-weight: bold; font-size: 50px; color: #00008b;");
-
-// shoes your recent score
-displayScore.textContent = "Your Score: " + localStorage.getItem("correct")
 
 // return to homepage button 
 function gotoLink(link){
@@ -30,21 +27,10 @@ function displayHighScores(){
         let highscoreEl = document.createElement("p");
         highscoreEl.textContent = highscoreString;
         document.querySelector("#highscores").appendChild(highscoreEl);
-        if (i === 5){
+        if (i >= 5){
             userScoresArray=[];
         }
     }
 }
-displayHighScores()
 
-// submit initials and score to the high scores
-subScore.addEventListener("click", function(){
-    let initials = document.getElementById("initials").value;
-    displayScore.textContent = initials + " " + userInScore;
-    userScoresArray.push({initials, userInScore})
-    let logIt = localStorage.setItem("userInScore", JSON.stringify(userScoresArray));
-    let initialsNow = JSON.parse(localStorage.getItem(initials, localStorage.getItem("correct")));
-    enterEl.classList.replace("visible", "hidden");
-    console.log(logIt);
-    console.log(initialsNow);
-})
+displayHighScores()
--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,7 +10,12 @@ let optionsContainer = document.querySelectorAll("#container");
 let correctAnswers = 0
 let timerInterval
 let correct = 0
-
+let enterInitials = document.querySelector(".enterScore");
+let submitBtn = document.getElementById("subScore");
+let displayScore = document.querySelector("#myScore");
+let userScoresArray = JSON.parse(localStorage.getItem("userInScore")) || []
+let userInScore = localStorage.getItem("correct");
+let enterEl = document.querySelector(".enterScore");
 
 
 // prevents right click
@@ -21,6 +26,7 @@ quizStart[0].addEventListener("click", function(){
   homePage.classList.replace("visible", "hidden");
   question[0].classList.replace("hidden", "visible");
   setTime();
+  localStorage.setItem("correct", correctAnswers)
 });
 
 // sets the timer at 75 secomds and stops at 0, taking you to high scores when quiz runs out of time
@@ -30,10 +36,10 @@ function setTime(){
     countTimer.textContent = "Seconds Left: " + timeLeft;
 
     if (timeLeft <= 0) {
-      alert("Time's up!");
+      alert("Time's up! Try Again!");
       clearInterval(timerInterval); 
-      window.location="highscores.html"
-      localStorage.setItem("correct", correctAnswers)}
+      window.location="index.html";
+      }
     }, 1000);
 }
 
@@ -61,7 +67,7 @@ answerKey[12].addEventListener("click", function(){
 // shows score and input for initials to add to high scores page
 answerKey[18].addEventListener("click", function(){
   question[4].classList.replace("visible", "hidden");
-  location.href="highscores.html";
+  enterInitials.classList.replace("hidden", "visible")
   if (timeLeft > 0){
     clearInterval(timerInterval);
   }
@@ -85,12 +91,29 @@ optionsContainer[i].addEventListener("click", function(e){
   }
 });}
 
+displayScore.textContent = "Your Score: " + localStorage.getItem("correct")
+
+
+// submit initials and score to the high scores
+submitBtn.addEventListener("click", function(){
+  let initials = document.getElementById("initials").value;
+  displayScore.textContent = initials + " " + localStorage.getItem("correct");
+  userScoresArray.push({initials, userInScore})
+  let logIt = localStorage.setItem("userInScore", JSON.stringify(userScoresArray));
+  let initialsNow = JSON.parse(localStorage.getItem(initials, localStorage.getItem("correct")));
+  enterEl.classList.replace("visible", "hidden");
+  console.log(logIt);
+  console.log(initialsNow);
+  window.location="highscores.html";
+})
+
+
 // setting style for quiz title, the high score link, timer, and start button
 quizTitle.setAttribute("style", "border-bottom: solid 2px #FF7F50; margin: 30px; font-weight: bold; font-size: 50px; color: #00008b;");
 countTimer.setAttribute("style", "color: #00008b;");
 quizStart[0].setAttribute("style", "border: solid #00008b 2px; background-color: #FF7F50; border-radius: 10% / 50%; color: #00008b;");
-// quizStart[1].setAttribute("style", "border: solid #00008b 2px; background-color: #FF7F50; border-radius: 10% / 50%; color: #00008b;");
 scoresLink.setAttribute("style", "color: #00008b; text-decoration: none;");
+submitBtn.setAttribute("style", "border: solid #00008b 2px; background-color: #FF7F50; border-radius: 10% / 50%; color: #00008b; padding: 10px 10px; width: 20%;");
 
 // sets styling for all answer buttons
 for (let i = 0; i < answerKey.length; i++){
